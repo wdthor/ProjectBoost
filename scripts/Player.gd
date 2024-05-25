63681b35b11715468620d7e65c03e286c230a572
wdthor <wdthor@proton.me>
2024-05-26 00:51:18 +0200

    2. Scripting Essentials * Calculating delta time * Incrementing a number

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,16 @@
 extends Node3D
 
+var timer:float = 0.0
+
+var count:int = 0
+
 func _ready() -> void:
 	print("Hello Godot 3D")
 
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		count += 1
+		print('Count : ', count)
+	#timer = timer + delta
+	#print(timer)
