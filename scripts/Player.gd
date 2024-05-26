9fa9bc4aa63b60ff5299ef045ccb5bcff563bce7
wdthor <wdthor@proton.me>
2024-05-26 09:55:20 +0200

    3. Controlling the player * Add a cylinder mesh, camera and light * Stop moving the camera when the parent (Player node) is moving * Rotate the cylinder on arrow keys pressed

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,16 +1,11 @@
 extends Node3D
 
-var timer:float = 0.0
-
-var count:int = 0
-
-func _ready() -> void:
-	print("Hello Godot 3D")
-
-
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		count += 1
-		print('Count : ', count)
-	#timer = timer + delta
-	#print(timer)
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
+	if Input.is_action_pressed()
+
