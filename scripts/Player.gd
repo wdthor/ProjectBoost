b4040ea20a63945c1d8a2c6dc71473ff652cf77e
wdthor <wdthor@proton.me>
2024-05-28 13:18:18 +0200

    6.RigidBody3D and Physics * Change Node3D from the Player Node to a RigidBody3D * Apply position and rotational force to the Player * lock linear_z, angular_x and angular_y params

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,16 @@
-extends Node3D
+extends RigidBody3D
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+		#Vector3.UP will move the object globally (from the global perspective)
+		#apply_central_force(Vector3.UP * delta * 1000.0)
+		#basis.y will move the object locally (from the node perspective)
+		
+		# apply_central_force() applies a directional force without affecting rotation.
+		apply_central_force(basis.y * delta * 1000.0)
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+		# apply_torque applies a rotational force without affecting position
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
 
